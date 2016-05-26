eb40ab37c6320cc4801d1838189e52743b933fae
YosuaSandy <sawachaki@gmail.com>
2016-05-27 08:55:45 +0900

    memasukan data suara ke database

--- app/src/main/java/com/example/sandy/quickcount/Keterangan.java
+++ app/src/main/java/com/example/sandy/quickcount/Keterangan.java
@@ -7,11 +7,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,17 +35,24 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Keterangan extends AppCompatActivity {
+public class Keterangan extends AppCompatActivity implements View.OnClickListener {
 
-    private String wilayah,no,cowo,cewe,jumlah;
+    private String wilayah,no,cowo,cewe,jumlah,id,suara1,suara2,rusak,sah;
     private TextView lakilaki,perempuan,total;
     public static final String DATA_URL = "http://192.168.42.125:8080/xampp/quickcount/peserta.php";
+    public static final String DATA_URL2 = "http://192.168.42.125:8080/xampp/quickcount/send.php";
+    public static final String TAG_ID = "ID";
     public static final String TAG_LAKILAKI = "Laki-laki";
     public static final String TAG_PEREMPUAN = "Perempuan";
     public static final String TAG_TOTAL = "Jumlah";
     public static final String TAG_URL = "kode_wilayah";
     public static final String TAG_NO = "no_tps";
     String tag_string_req = "req_data";
+    EditText calon1;
+    EditText calon2;
+    EditText suaraTidakSah;
+    TextView suaraSah;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,17 +73,23 @@ public class Keterangan extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        button1 = (Button) findViewById(R.id.send);
+        button1.setOnClickListener(this);
 
         lakilaki = (TextView) findViewById(R.id.lakilaki);
         perempuan = (TextView) findViewById(R.id.perempuan);
         total = (TextView) findViewById(R.id.total);
-
+        calon1= (EditText) findViewById(R.id.edtlabel3);
+        calon2 = (EditText) findViewById(R.id.edtlabel4);
+        suaraTidakSah = (EditText) findViewById(R.id.edtlabel6);
+        suaraSah = (TextView) findViewById(R.id.edtlabel5);
+        TotalSuaraSah();
 
         String nomor= getIntent().getExtras().getString("nomer");
         no = nomor;
         String kode = getIntent().getExtras().getString("wilayah");
         wilayah = kode;
-        StringRequest jsonArrayRequest = new StringRequest(Request.Method.POST,DATA_URL, new Response.Listener<String>() {
+        final StringRequest jsonArrayRequest = new StringRequest(Request.Method.POST,DATA_URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Log.d("flag", response.toString());
@@ -87,12 +104,14 @@ public class Keterangan extends AppCompatActivity {
                            String cowok = jsonObject.getString(TAG_LAKILAKI);
                             String cewek= jsonObject.getString(TAG_PEREMPUAN);
                             String sum = jsonObject.getString(TAG_TOTAL);
+                            String no_id = jsonObject.getString(TAG_ID);
 //                            Log.d("flag", jsonObject.toString());
 
                             cowo = cowok;
                             cewe = cewek;
                             jumlah = sum;
-
+                            id =no_id;
+                            Log.d("flag", "get "+ id );
 
                         }
 //                        Log.d("flag", response.toString());
@@ -176,6 +195,106 @@ public class Keterangan extends AppCompatActivity {
 
         }
 
+    }
+
+    private String addNumbers() {
+        int number1;
+        int number2;
+        if(calon1.getText().toString() != "" && calon1.getText().length() > 0) {
+            number1 = Integer.parseInt(calon1.getText().toString());
+        } else {
+            number1 = 0;
+        }
+        if(calon2.getText().toString() != "" && calon2.getText().length() > 0) {
+            number2 = Integer.parseInt(calon2.getText().toString());
+        } else {
+            number2 = 0;
+        }
+
+        return Integer.toString(number1 + number2);
+    }
+
+    private void TotalSuaraSah () {
+        calon1.addTextChangedListener(new TextWatcher() {
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+//                 TODO Auto-generated method stub
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                suaraSah.setText(addNumbers());
+            }
+
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        calon2.addTextChangedListener(new TextWatcher() {
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // TODO Auto-generated method stub
+
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                suaraSah.setText(addNumbers());
+
+            }
+
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+    }
+
+    public void onClick(View v) {
+        suara();
+
+    }
+
+    private void suara() {
+        suara1 = calon1.getText().toString().trim();
+        suara2 = calon2.getText().toString().trim();
+        rusak = suaraTidakSah.getText().toString().trim();
+        sah = suaraSah.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, DATA_URL2,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.trim().equals("success")){
+                            Toast.makeText(Keterangan.this,"penyimpanan sukses",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(Keterangan.this,response,Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Keterangan.this,error.toString(),Toast.LENGTH_LONG ).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<String,String>();
+                map.put("id_tps",id);
+                map.put("calon1",suara1);
+                map.put("calon2",suara2);
+                map.put("rusak",rusak);
+                map.put("sah",sah);
+                return map;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
 
